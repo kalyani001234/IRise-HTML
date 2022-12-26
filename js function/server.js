85eaa1ddba365e9567ee779579b17b6192dca634
kalyani001234 <kalyanishenwai303@gmail.com>
2022-12-26 21:35:53 +0530

    Create server.js
    js function assignment

---
+++ js function/server.js
@@ -0,0 +1,15 @@
+var car = {name:'Swift',company:'Suzuki',color:'Silver',model:'VXI'}
+console.log(car);
+
+var state;
+console.log(state);
+
+function greetings(){
+    var getGreet = "Hello! Kalyani";
+    var getName = "Good";
+    var getString = "Morning";
+    console.log(getGreet);
+    console.log(getName);
+    console.log(getString);   
+}
+greetings();
